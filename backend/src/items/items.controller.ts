ff21c3f451f617ed3619c1b248a6ced0e98e0290
Rudra <rudrahimangshoo2020@gmail.com>
2024-06-05 02:28:30 +0600

    crud

--- backend/src/items/items.controller.ts
+++ backend/src/items/items.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { ItemsService } from './items.service';
+import { CreateItemDto } from './create-item.dto';
 
 @Controller('items')
-export class ItemsController {}
+export class ItemsController {
+  constructor(private readonly itemsService: ItemsService) {}
+  @Get()
+  findAll() {
+    return this.itemsService.findAll();
+  }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.itemsService.findone(id);
+  }
+  @Post(':id')
+  update(@Param(':id') id: string, @Body() updateItemDto: CreateItemDto) {
+    return this.itemsService.update(id, updateItemDto);
+  }
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.itemsService.delete(id);
+  }
+}
--- backend/src/items/items.service.ts
+++ backend/src/items/items.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { CreateItemDto } from './create-item.dto';
 
 @Injectable()
-export class ItemsService {}
+export class ItemsService {
+  private items = [];
+
+  findAll() {
+    return this.items;
+  }
+
+  findone(id: string) {
+    return this.items.find((item) => item.id === id);
+  }
+
+  create(CreateItemDto: CreateItemDto) {
+    const newItem = {
+      id: Date.now().toString(),
+      ...CreateItemDto,
+    };
+    this.items.push(newItem);
+    return newItem;
+  }
+  update(id: string, updateItemDto: CreateItemDto) {
+    const itemIndex = this.items.findIndex((item) => item.id === id);
+    if (itemIndex > -1) {
+      this.items[itemIndex] = { id, ...updateItemDto };
+      return this.items[itemIndex];
+    }
+    return null;
+  }
+  delete(id: string) {
+    const itemIndex = this.items.findIndex((item) => item.id === id);
+    if (itemIndex > -1) {
+      const deletedItem = this.items.splice(itemIndex, 1);
+      return deletedItem[0];
+    }
+    return null;
+  }
+}
