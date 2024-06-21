eed5ad0293089a95832c4e79eae5aa163d18b279
Rudra Pramanik <rudrahimangshoo2020@gmail.com>
2024-06-21 23:08:47 +0600

    re-initialize

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ItemsModule } from './items/items.module';
 
 @Module({
-  imports: [ItemsModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
--- index.js
+++
@@ -1,8 +0,0 @@
-function anagram(input1, input2){
-let output = ''
-if(input1.length === inputt2.length){
-  let str 
-}
-
-}
-//
