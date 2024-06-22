dba8fafb3806eaf08ec9ba46566a51a93bf2136e
Rudra Pramanik <rudrahimangshoo2020@gmail.com>
2024-06-23 00:57:24 +0600

    docker-added-module-added

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostModule } from './modules/post/post.module';
+import { PostModules } from './modules/posts/post.module';
 
 @Module({
-  imports: [PostModule],
+  imports: [PostModule, PostModules],
   controllers: [AppController],
   providers: [AppService],
 })
