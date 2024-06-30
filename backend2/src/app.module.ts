70eebb9439a470303e1b4b6bb2d4ce1a72f91b9c
Rudra <rudrahimangshoo2020@gmail.com>
2024-06-30 19:12:50 +0600

    added-contoller

--- backend2/src/app.module.ts
+++ backend2/src/app.module.ts
@@ -2,15 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostModule } from './modules/post/post.module';
-import { AuthModule } from './modules/auth/auth.module';
+import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { CatsModule } from './cats/cats.module';
 import { OfficesModule } from './offices/offices.module';
-import { FeedbacksModule } from './feedbacks/feedbacks.module';
 import { FeedbacksModule } from './feedbacks/feedbacks.module';
 
 @Module({
-  imports: [PostModule, AuthModule, UsersModule, CatsModule, OfficesModule, FeedbacksModule],
+  imports: [PostModule, AuthModule, UsersModule, OfficesModule, FeedbacksModule],
   controllers: [AppController],
   providers: [AppService],
 })
