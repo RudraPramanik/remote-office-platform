c70636650f420f73bdd2c77a0a3a7e9ae9230987
Rudra <rudrahimangshoo2020@gmail.com>
2024-07-01 01:37:20 +0600

    auht-service

--- backend2/src/auth/auth.service.ts
+++ backend2/src/auth/auth.service.ts
@@ -9,18 +9,28 @@ export class AuthService {
     private jwtService: JwtService
   ) {}
 
-  async signIn(
-    username: string,
-    pass: string,
-  ): Promise<{ access_token: string }> {
-
-    const user = await this.usersService.findOne(username);
-    if (user?.password !== pass) {
-      throw new UnauthorizedException();
-    }
-    const payload = { sub: user.userId, username: user.username };
-    return {
-      access_token: await this.jwtService.signAsync(payload),
-    };
+  async signUp(){
+    return {message:'signup successful'}
   }
+  async signIn(){
+    return ''
+  }
+  async signOut(){
+    return ''
+  }
+
+  // async signIn(
+  //   username: string,
+  //   pass: string,
+  // ): Promise<{ access_token: string }> {
+
+  //   const user = await this.usersService.findOne(username);
+  //   if (user?.password !== pass) {
+  //     throw new UnauthorizedException();
+  //   }
+  //   const payload = { sub: user.userId, username: user.username };
+  //   return {
+  //     access_token: await this.jwtService.signAsync(payload),
+  //   };
+  // }
 }
